7ce3964fb0ddb3d8de8243b196723699b111e4d7
shuvookd <shuvokd1210@gmail.com>
2022-11-01 01:36:04 +0600

    Team

--- 71A.Way_Too_Long_Words.cpp
+++ 71A.Way_Too_Long_Words.cpp
@@ -1,3 +1,10 @@
+/*
+Input
+The first line contains an integer n (1 ≤ n ≤ 100). Each of the following n lines contains one word. All the words consist
+of lowercase Latin letters and possess the lengths of from 1 to 100 characters.
+Output
+Print n lines. The i-th line should contain the result of replacing of the i-th word from the input data.
+*/
 #include <iostream>
 using namespace std;
 int main()
