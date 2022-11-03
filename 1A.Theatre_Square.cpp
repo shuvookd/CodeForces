a691cb8f9686c9aa8c4087eca909da81f1c43043
shuvookd <shuvokd1210@gmail.com>
2022-11-03 21:22:27 +0600

    A1 Theatre Square addedd

--- 1A.Theatre_Square.cpp
+++ 1A.Theatre_Square.cpp
@@ -1,6 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"hello";
-    return 0;
+int n, m, a;
+long long x, y, sum;
+int main() {
+    cin >> n >> m >> a;
+    x = n / a;
+    y = m / a;
+    if (n % a != 0) {
+        x = x + 1;
+    }
+    if (m % a != 0) {
+        y = y + 1;
+    }
+    sum = x * y;
+    cout << sum;
 }
