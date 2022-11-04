993bd22348c3fc82a33c5388dbb2a2dacccec566
shuvookd <shuvokd1210@gmail.com>
2022-11-05 02:00:04 +0600

    158A.Next_Round added

---
+++ 158A.Next_Round.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k, a, b = 0, mas[10000];
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        cin >> a;
+        mas[i] = a;
+    }
+    for (int j = 1; j <= n; j++) {
+        if (mas[j] >= mas[k] && mas[j] > 0) {
+            b++;
+        }
+    }
+    cout << b;
+    return 0;
+}
