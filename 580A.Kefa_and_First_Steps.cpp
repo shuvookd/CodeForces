44d0f7ee4c42ad404d16fb2b384c3b735263f691
shuvookd <shuvokd1210@gmail.com>
2022-12-10 19:47:34 +0600

    148.Insomnia_cure added

---
+++ 580A.Kefa_and_First_Steps.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    string a[n];
+    for(int i=1; i<=n; i++){
+        cin>>a[i];
+    }
+    cout<<a;
+return 0;
+}
